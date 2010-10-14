19b98247937f3b2bc374d8bebd0b359e6ff32c99
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-10-14 20:06:08 +0400

    pretty comments

--- js/websocket.js
+++ js/websocket.js
@@ -1,5 +1,5 @@
+// emulating console object to avoid errors in nedobrowsers
 if ('undefined' == typeof(console)) {
-	// emulating console object to avoid errors
 	console = {
 		  log : function(msg) { },
 		error : function(msg) { },
@@ -8,22 +8,33 @@ if ('undefined' == typeof(console)) {
 }
 
 /**
- * Data encoder for WebSocket
+ * WebSocket connection object
+ * You can set the settings via constructor argument
+ *
+ * var ws = new WebSocketConnection( {
+ *   jspath: '/javascript/',                    path to your javascript directory from the web (default value is "/js/")
+ *
+ *   url: {                                     urls list object, you can use string as value to set all variants
+ *                                              also you can set urls to methods you allowed, other will not be used
+ *
+ *     websocket: 'ws://test/test'              - websocket connection url
+ *   },
+ *                                              events handlers (all handlers can be assigned to object manually):
+ *
+ *   onReady      = function() { },             - connection object is ready for use
+ *   onConnected  = function() { },             - connected to server
+ *   onDisconcted = function() { },             - disconnected
+ *   onMessage    = function(message) { },      - new message from server received
+ *   onError      = function(error) { },        - error
+ * } );
+ *
+ *                                              object methods:
+ *
+ * if (ws.isConnected()) {                      - returns true if connection to server is opened
+ *   ws.send(somedata);                         - send somedata to server
+ *   ws.close();                                - close the connection
+ * }
  */
-function WebSocketEncodeData(data) {
-	var query = [];
-	
-	if (data instanceof Object) {
-		for (var k in data) {
-			query.push(encodeURIComponent(k) + '=' + encodeURIComponent(data[k]));
-		}
-		
-		return query.join('&');
-	} else {
-		return encodeURIComponent(data);
-	}
-}
-
 WebSocketConnection = function() {
 	
 	var provider = null;	
@@ -57,7 +68,7 @@ WebSocketConnection = function() {
 		provider.send(data);
 	}
 	
-	this.connected = function() {
+	this.isConnected = function() {
 		return (
 			provider
 			&& provider.OPEN === provider.readyState
@@ -68,18 +79,22 @@ WebSocketConnection = function() {
 // Provider event handlers
 // ---------------------------------------------------------	
 
-	var onProviderOpen = function() {	
+	// connection is opened
+	var onProviderOpen = function() {
 		self.onConnected();
 	}
 	
+	// connection is closed
 	var onProviderClose = function() {
 		self.onDisconnected();
 	}
 	
+	// new message received
 	var onProviderMessage = function(message) {
 		self.onMessage(message);
 	}
 	
+	// error ;(
 	var onProviderError = function(error) {
 		self.onError(error);
 	}
@@ -91,13 +106,14 @@ WebSocketConnection = function() {
 	var initProvider = function(url) {
 		if ('undefined' === typeof(WebSocketProvider)) {
 			self.onError({
-				description: 'No provider defined',
+				description: 'No provider is defined',
 				err: 1
 			});
 
 			return false;
 		}
 
+		// creating provider object
 		try {
 			provider = new WebSocketProvider(url);
 		} catch (e) {
@@ -106,12 +122,14 @@ WebSocketConnection = function() {
 			return false;
 		}
 
+		// assigning handler functions
 		provider.onopen    = onProviderOpen;
 		provider.onmessage = onProviderMessage;
 		provider.onclose   = onProviderClose;
 		
 		provider.onerror   = self.onError;
 		
+		// firing onReady event
 		self.onReady();
 
 		return true;
@@ -128,7 +146,10 @@ WebSocketConnection = function() {
 		console.error('url is not defined in settings');
 		return false;
 	}
+	
+	// @todo check for string in 'url' value to assign every provider with it
 		
+	// checking jspath, default is "/js/"
 	settings['jspath'] = settings['jspath'] || '/js/';
 	
 	// Overriding event handlers from settings
@@ -136,9 +157,12 @@ WebSocketConnection = function() {
 	for (var key in settings) {
 		if (
 			key.length > 1
+			// onXXXX ?
 			&& key.substring(0, 2) === 'on'
+			// do we have this public method?
 			&& key in this
 		) {
+			// overriding
 			this[key] = settings[key];
 		}
 	}
@@ -152,6 +176,7 @@ WebSocketConnection = function() {
 	// Check the connection type and initialize provider
 	if (
 		'WebSocket' in window
+		// only if settings.url.websocket is defined
 		&& 'websocket' in settings['url']
 	) {
 		// native WebSocket found
@@ -160,28 +185,47 @@ WebSocketConnection = function() {
 	}
 	
 	if (!result) {
-		// Building WebSocket skeleton
+		// Building WebSocketProvider skeleton similar to WebSocket
 		WebSocketProvider = function(url) { 
 			this.URL = url;
 			this.bufferedAmount = 0;
 			this.readyState = 2;
 		};
 		
+		// default constants
 		WebSocketProvider.prototype.CONNECTING = 0;
 		WebSocketProvider.prototype.OPEN       = 1;
 		WebSocketProvider.prototype.CLOSED     = 2;
 		
+		// event handlers
 		WebSocketProvider.prototype.onopen    = function() {};
 		WebSocketProvider.prototype.onmessage = function() {};
 		WebSocketProvider.prototype.onclose   = function() {};
 		WebSocketProvider.prototype.onerror   = function() {};
 
+		// providers have to override these methods:
 		WebSocketProvider.prototype.close     = function() {};
 		WebSocketProvider.prototype.send      = function(data) {};
-		
-		// @todo check flash
-		result = initProvider(settings['url']['websocket']);
 	}
+	
+	// @todo check other providers
 
 	return result;
+}
+
+/**
+ * Data encoder for WebSocket
+ */
+function WebSocketEncodeData(data) {
+	var query = [];
+	
+	if (data instanceof Object) {
+		for (var k in data) {
+			query.push(encodeURIComponent(k) + '=' + encodeURIComponent(data[k]));
+		}
+		
+		return query.join('&');
+	} else {
+		return encodeURIComponent(data);
+	}
 }

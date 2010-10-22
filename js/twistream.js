226faf9aa1371ca38488d77610dc233e49c4fcc7
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-10-23 00:29:50 +0400

    some js code restructure

--- js/twistream.js
+++ js/twistream.js
@@ -1,45 +1,92 @@
 (function($) {
 
-	$.twistream = function() {
-		var settings = arguments[0] || { };
-
+	$.twistream = function(s) {
 		var self = this;
+	
+		/**
+		 * Settings for application
+		 */
+		var settings = $.extend( {
+			// default settings
+			feed: 'body'
+		}, s);
+		
+		/**
+		 * Object to collect useful places
+		 */
+		var places = {
+			stopplay: $('#sp img'),
+			header: $('#top')
+		};
 
-		var map = null;
+		/**
+		 * Object for the stream filter params
+		 */
+		var params = {
+			mapZoom: 10,
+			// Moscow coordinates by default
+			mapCenterLng: 55.755786121111, 
+			mapCenterLat: 37.617633343333
+		}
 
+		/**
+		 * Websocket connection
+		 * @type {Object}
+		 */
 		var connection = null;
 
+		/**
+		 * Tweets array
+		 * @type {Array}
+		 */
 		var tweets = [];
+		
+		/**
+		 * Markers array
+		 * @type {Array}
+		 */
 		var markers = [];
 
+		/**
+		 * Timer to hide old log message
+		 */
 		var logtimer = null;
+		
+		/**
+		 * Timer to handle params is changed
+		 */
 		var pchangedtimer = null;
 
+		/**
+		 * Paused?
+		 * @type {Boolean} 
+		 */
 		var paused = false;
 
-		var topElement = $('#top');
-		var logger = $('#logger');
-		var stopplay = $('#sp img');
-
-		topElement.css( { opacity: 0.8 } );
-		$('#tweets').css( { opacity: 0.9 } );
-
+		/**
+		 * Update the state of pause/play button image
+		 */
 		var updateState = function() {
 			if (paused) {
-				stopplay.attr( {
+				places.stopplay.attr( {
 					src: 'img/play.png',
 					title: 'resume'
 				} );
 			} else {
-				stopplay.attr( {
+				places.stopplay.attr( {
 					src: 'img/pause.png',
 					title: 'pause'
 				} );
 			}
 		}
 
+		/**
+		 * Toggle stream state
+		 */
 		var togglePause = function() {
 			paused = !paused;
+			
+			// updating image on the button
 			updateState();
 
 			if (paused) {
@@ -48,23 +95,33 @@
 				log('Resuming');
 			}
 
+			// apply params
 			changeParams();
 		}
 
-		var _log = function(message) {
-			logger
-				.html(message)
-				.fadeIn('slow');
-
-			logtimer = setTimeout(function() {
-				logger.fadeOut('slow');
-			}, 2000)
-		}
-
+		/**
+		 * Log some message in logger place (settings.logger)
+		 * @param {string} message Message to log
+		 */
 		var log = function(message) {
+			if (!('logger' in places)) {
+				return;
+			}
+		
+			var _log = function(message) {
+				places.logger
+					.html(message)
+					.fadeIn('slow');
+
+				logtimer = setTimeout(function() {
+					places.logger.fadeOut('slow');
+				}, 2000)
+			}
+		
 			if (null != logtimer) {
 				clearTimeout(logtimer);
-				logger.fadeOut('fast', function() {
+				
+				places.logger.fadeOut('fast', function() {
 					_log(message);
 				} );
 			} else {
@@ -72,58 +129,10 @@
 			}
 		}
 
-		$('input', topElement)
-			.mouseover( function() {
-				topElement.animate({opacity: 1});
-			} )
-			.mouseout( function() {
-				topElement.animate({opacity: 0.8});
-			} );
-
-		var mapCenter = new google.maps.LatLng(-34.397, 150.644);
-		var mapZoom   = 10;
-
-		// @todo don't ask user for geo if settings are found in localStorage
-
-		if ('localStorage' in window) {
-			mapZoom = parseInt(localStorage.getItem('map_zoom') || mapZoom);
-
-			mapCenter = new google.maps.LatLng(
-				parseFloat(localStorage.getItem('map_lng') || mapCenter.lng()),
-				parseFloat(localStorage.getItem('map_lat') || mapCenter.lat())
-			);
-		}
-
-//		if ('map' in settings) {
-			map = new google.maps.Map(
-				document.getElementById('maps'), {
-					zoom: mapZoom,
-					center: mapCenter,
-					disableDefaultUI: true,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				}
-			);
-//		}
-
-		var checkVisibleTweets = function() {
-			if (tweets.length === 0) {
-				// not impossible O.o
-				return;
-			}
-
-			var el = tweets[0].element[0];
-
-			if (el.offsetTop > window.innerHeight) {
-				el = tweets.shift();
-
-				el.element.remove();
-
-				if (null != el.marker) {
-					el.marker.setMap(null);
-				}
-			}
-		}
-
+		/**
+		 * Add tweet to the feed
+		 * @param {!Object} tweetObj Tweet object
+		 */
 		var addTweet = function(tweetObj) {
 			if (
 				null === tweetObj
@@ -132,68 +141,67 @@
 			) {
 				return;
 			}
+			
+			var marker = null;
 
-			// geo
+			if ('map' in places) {
+				// prepare and set the tweet marker on the map
 
-			var markerLat = null;
-			var markerLng = null;
+				var markerLat = null;
+				var markerLng = null;
 
-			if (
-				'geo' in tweetObj.tweet
-				&& null !== tweetObj.tweet.geo
-			) {
-				markerLat = tweetObj.tweet.geo.coordinates[0];
-				markerLng = tweetObj.tweet.geo.coordinates[1];
-			} else
-			if (
-				'place' in tweetObj.tweet
-				&& null !== tweetObj.tweet.place
-			) {
-				var coord = tweetObj.tweet.place.bounding_box.coordinates.shift();
+				// checking the accurate geolocation in 'geo' tweet object
+				if (
+					'geo' in tweetObj.tweet
+					&& null !== tweetObj.tweet.geo
+				) {
+					markerLat = tweetObj.tweet.geo.coordinates[0];
+					markerLng = tweetObj.tweet.geo.coordinates[1];
+				} else
+				// checking for inaccurate 'place' object in tweet
+				if (
+					'place' in tweetObj.tweet
+					&& null !== tweetObj.tweet.place
+				) {
+					var coord = tweetObj.tweet.place.bounding_box.coordinates.shift();
 
-				if (coord) {
-					while (true) {
-						var poly = coord.shift();
+					if (coord) {
+						while (true) {
+							var poly = coord.shift();
 
-						if (poly) {
-							if (null === markerLng) {
-								markerLng = poly[0]
+							if (poly) {
+								if (null === markerLng) {
+									markerLng = poly[0]
+								} else {
+									markerLng = (markerLng + poly[0]) / 2;
+								}
+
+								if (null === markerLat) {
+									markerLat = poly[1];
+								} else {
+									markerLat = (markerLat + poly[1]) / 2;
+								}
 							} else {
-								markerLng = (markerLng + poly[0]) / 2;
+								break;
 							}
-
-							if (null === markerLat) {
-								markerLat = poly[1];
-							} else {
-								markerLat = (markerLat + poly[1]) / 2;
-							}
-						} else {
-							break;
 						}
 					}
-				} else {
-					console.info('coords not found in tweet object');
 				}
-			} else {
-				console.info('nothing is set in tweet coords');
-				console.dir(tweetObj.tweet);
-			}
 
-			if (
-				null != markerLat
-				&& null != markerLng
-			) {
-				var marker = new google.maps.Marker( {
-					position: new google.maps.LatLng(
-						markerLat,
-						markerLng
-					),
-					map: map,
-					title: 'Tweet',
-					icon: 'img/marker.gif'
-				} );
-			} else {
-				var marker = null;
+				if (
+					null != markerLat
+					&& null != markerLng
+				) {
+					var marker = new google.maps.Marker( {
+						position: new google.maps.LatLng(
+							markerLat,
+							markerLng
+						),
+						map: places.map,
+						title: 'Tweet',
+						icon: 'img/marker.gif'
+					} );
+				}
 			}
 
 			// tweet
@@ -252,7 +260,7 @@
 				.appendTo(tweet);
 
 			tweet
-				.prependTo('#tweets')
+				.prependTo(places.feed)
 				.slideDown('fast', 'linear');
 
 			tweets.push({
@@ -260,7 +268,24 @@
 				marker: marker
 			});
 
-			checkVisibleTweets();
+			// check for visible tweets
+			
+			if (tweets.length === 0) {
+				// not impossible O.o
+				return;
+			}
+
+			var el = tweets[0].element[0];
+
+			if (el.offsetTop > window.innerHeight) {
+				el = tweets.shift();
+
+				el.element.remove();
+
+				if (null != el.marker) {
+					el.marker.setMap(null);
+				}
+			}
 		}
 
 		var changeParams = function() {
@@ -280,7 +305,7 @@
 				track: 'google'
 			};*/;
 
-			var mapBounds = map.getBounds();
+			var mapBounds = places.map.getBounds();
 			if (null != mapBounds) {
 				var mapBoundsNE = mapBounds.getNorthEast();
 				var mapBoundsSW = mapBounds.getSouthWest();
@@ -333,31 +358,88 @@
 
 			pchangedtimer = setTimeout(changeParams, 2000);
 		}
+		
+// ---------------------------------------------------------------------------------
+// Init
+// ---------------------------------------------------------------------------------
 
-		var mapCoordsChangedHandler = function() {
-			if (
-				'localStorage' in window
-				&& null != map
-			) {
-				var mapCenter = map.getCenter();
+		if ('feed' in settings) {
+			places.feed = $(settings.feed);
+		}
 
-				localStorage.setItem('map_lat', mapCenter.lat());
-				localStorage.setItem('map_lng', mapCenter.lng());
-				localStorage.setItem('map_zoom', map.getZoom());
+		if ('logger' in settings) {
+			places.logger = $(settings.logger);
+		}
+		
+		// make keywords input item fading on mouseout/mousein
+		$('input', places.header)
+			.mouseover( function() {
+				places.header.animate({opacity: 1});
+			} )
+			.mouseout( function() {
+				places.header.animate({opacity: 0.8});
+			} );
+
+// ---------------------------------------------------------------------------------
+		
+		places.header.css( { opacity: 0.8 } );
+		places.feed.css(   { opacity: 0.9 } );
+		
+		places.stopplay.click(togglePause);
+		
+// ---------------------------------------------------------------------------------
+// Some code to work with google maps
+// ---------------------------------------------------------------------------------
+			
+		if ('localStorage' in window) {
+			// maybe we already have coordinates in localStorage?
+			
+			params.mapZoom      = parseInt(   localStorage.getItem('map_zoom') || params.mapZoom );
+			params.mapCenterLng = parseFloat( localStorage.getItem('map_lng')  || params.mapCenterLng );
+			params.mapCenterLat = parseFloat( localStorage.getItem('map_lat')  || params.mapCenterLag );
+		}
+				
+		if ('map' in settings) {
+			var mapCenter = new google.maps.LatLng(
+				params.mapCenterLat, 
+				params.mapCenterLng
+			);
+			
+			var mapElement = $(settings.map);
+			
+			if (0 === mapElement.length) {
+				return false;
+			}
+		
+			places.map = new google.maps.Map(mapElement[0], {
+				zoom:             params.mapZoom,
+				center:           mapCenter,
+				disableDefaultUI: true,
+				mapTypeId:        google.maps.MapTypeId.ROADMAP
+			} );
+		
+			var mapCoordsChangedHandler = function() {
+				if (
+					'localStorage' in window
+				) {
+					var mapCenter = places.map.getCenter();
+
+					localStorage.setItem('map_lat', mapCenter.lat());
+					localStorage.setItem('map_lng', mapCenter.lng());
+					localStorage.setItem('map_zoom', places.map.getZoom());
+				}
+
+				paramsChangedHandler();
+			}
+			
+			places.map.bounds_changed = function() {
+				mapCoordsChangedHandler();
 			}
 
-			paramsChangedHandler();
+			places.map.center_changed = function() {
+				mapCoordsChangedHandler();
+			}
 		}
-
-		map.bounds_changed = function() {
-			mapCoordsChangedHandler();
-		}
-
-		map.center_changed = function() {
-			mapCoordsChangedHandler();
-		}
-
-		stopplay.click(togglePause);
 
 		if ('geolocation' in navigator) {
 			navigator.geolocation.getCurrentPosition( function(position) {
@@ -365,23 +447,31 @@
 					'coords' in position
 					&& 'latitude' in position.coords
 				) {
-					//log('Initial coordinates changed');
+					log('Center coordinates changed by browser geolocation result');
 
-					map.setCenter(
-						new google.maps.LatLng(
-							position.coords.latitude,
-							position.coords.longitude
-						)
-					);
+					if ('map' in places) {
+						places.map.setCenter(
+							new google.maps.LatLng(
+								position.coords.latitude,
+								position.coords.longitude
+							)
+						);
+					}
+					
+					changeParams();
 				}
-
-				//changeParams();
 			} );
 		} else {
 			changeParams();
 		}
 
+// ---------------------------------------------------------------------------------
+
 		updateState();
+
+// ---------------------------------------------------------------------------------
+// Something to test and for a makeup
+// ---------------------------------------------------------------------------------
 
 		if (
 			'makeup' in settings
@@ -389,7 +479,11 @@
 		) {
 			addTweet( {
 				tweet: {
-					text: 'Testing @silentroach tweet with many words and lines O.o great for makeup fixing and something to test. Don\'t know where to found enough words to make tweet bigger in height, so just test some links http://yfrog.com/3r7mzqj and a #hashtag and #hashtag2 and big links like this http://habrahabr.ru/blogs/gadgets/106319/ that\'s all.',
+					text: 'Testing @silentroach tweet with many words and lines O.o great ' +
+						'for makeup fixing and something to test. Don\'t know where to found ' +
+						'enough words to make tweet bigger in height, so just test some links ' +
+						'http://yfrog.com/3r7mzqj and a #hashtag and #hashtag2 and big links ' +
+						'like this http://habrahabr.ru/blogs/gadgets/106319/ that\'s all.',
 					user: {
 						screen_name: 'silentroach',
 						profile_image_url: 'http://a2.twimg.com/profile_images/24736482/a_a09b53a_normal.jpg'
